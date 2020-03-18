66c8fab0f9a3c9ac28c651dfcd849f92ae44ca0a
clemdetry <c.detry@student.maastrichtuniversity.nl>
2020-03-18 16:38:38 +0100

    upload second game mode method

--- core/src/code/Game/PuttingSimulator.java
+++ core/src/code/Game/PuttingSimulator.java
@@ -4,6 +4,11 @@ import code.Board.*;
 import code.Physics.EulerSolver;
 import code.Physics.PhysicsEngine;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PuttingSimulator extends EulerSolver {
 
     private PuttingCourse course;
@@ -31,5 +36,30 @@ public class PuttingSimulator extends EulerSolver {
 
     public void take_shot(Vector2d initial_ball_acceleration){
         acceleration = initial_ball_acceleration;
+    }
+
+    public void take_shot_mode2(File file){
+
+        double x_velocity = 0;
+        double y_velocity = 0;
+        String currentLine;
+        String[] tab;
+        BufferedReader bufferedReader;
+        FileReader fileReader;
+
+        try {
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+            currentLine = bufferedReader.readLine();
+            tab = currentLine.split(" ");
+            x_velocity = Double.parseDouble(tab[0]);
+            y_velocity = Double.parseDouble(tab[1]);
+            bufferedReader.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        Vector2d p = new Vector2d(x_velocity, y_velocity);
+        take_shot(p);
     }
 }
