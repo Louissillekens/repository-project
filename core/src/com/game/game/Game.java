c37d95d76354c0a92b0baae22c8004cba5347249
Louissillekens <56159154+Louissillekens@users.noreply.github.com>
2020-03-18 18:41:53 +0000

    Update Game.java

--- core/src/com/game/game/Game.java
+++ core/src/com/game/game/Game.java
@@ -1,5 +1,9 @@
 package com.game.game;
 
+import code.Board.PuttingCourse;
+import code.Board.Vector2d;
+import code.Game.PuttingSimulator;
+import code.Physics.PhysicsEngine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -11,97 +15,63 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Vector3;
 
-public class Game extends ApplicationAdapter implements InputProcessor {
+public class Game extends ApplicationAdapter{
 
-	private PerspectiveCamera cam;
+	private PerspectiveCamera camera;
 	private ModelBatch modelbatch;
 	private ModelBuilder modelbuilder;
-	private Model box;
-	private ModelInstance modelInstance;
+	private Model course;
+	private ModelInstance courseInstance;
 	private Environment environment;
 	public static Vector3 origin = new Vector3(0f,0f,0f);
+	private PuttingSimulator simulator;
 
 	@Override
 	public void create () {
 
-		cam = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(0f, 0f, 5f);
-		cam.lookAt(origin);
-		cam.near = 0.1f;
-		cam.far = 250f;
+		double[][] height_map = new double[10][10];
+		for(int i = 0 ; i < height_map.length ; i++){
+			for(int j = 0 ; j < height_map[0].length ; j++){
+				height_map[i][j] = 1;
+			}
+		}
 
-		modelbatch = new ModelBatch();
-		modelbuilder = new ModelBuilder();
+		double[][] friction_map = new double[10][10];
+		for(int i = 0 ; i < friction_map.length ; i++){
+			for(int j = 0 ; j < friction_map[0].length ; j++){
+				friction_map[i][j] = 0.131;
+			}
+		}
 
-		box = modelbuilder.createBox(2f,2f,2f, new Material(ColorAttribute.createDiffuse(Color.BLUE)),
-				VertexAttributes.Usage.Position|VertexAttributes.Usage.Normal);
+		Vector2d start = new Vector2d(1,1);
+		Vector2d hole = new Vector2d(8,8);
+		PuttingCourse game_course = new PuttingCourse(height_map, friction_map, start, hole);
+		PhysicsEngine game_engine = new PhysicsEngine();
+		simulator = new PuttingSimulator(game_course, game_engine);
 
-		modelInstance = new ModelInstance(box,0,0,0);
-		environment = new Environment();
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.8f,0.8f,0.8f, 1f));
 
-		Gdx.input.setInputProcessor(this);
+
+		camera = new PerspectiveCamera(
+				75,
+				Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight());
+
+		// Move the camera 5 units back along the z-axis and look at the origin
+		camera.position.set(0f,0f,7f);
+		camera.lookAt(0f,0f,0f);
+
+		// Near and Far (plane) represent the minimum and maximum ranges of the camera in, um, units
+		camera.near = 0.1f;
+		camera.far = 300.0f;
+		
+
 	}
 
 	@Override
 	public void render () {
 
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
 
-		cam.update();
-		modelbatch.begin(cam);
-		modelbatch.render(modelInstance);
-		modelbatch.end();
 	}
 
-	@Override
-	public boolean keyDown(int keycode) {
-		if(keycode == Input.Keys.LEFT)
-			cam.rotateAround(new Vector3(0f,0f,0f), new Vector3(0f,1f,0f), 1f);
-		if(keycode == Input.Keys.RIGHT)
-			cam.rotateAround(new Vector3(0f,0f,0f), new Vector3(0f,1f,0f), -1f);
-		if(keycode == Input.Keys.UP)
-			cam.rotateAround(new Vector3(0f,0f,0f), new Vector3(1f,0f,0f), 1f);
-		if(keycode == Input.Keys.DOWN)
-			cam.rotateAround(new Vector3(0f,0f,0f), new Vector3(1f,0f,0f), -1f);
 
-
-		return true;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
-	}
 }
