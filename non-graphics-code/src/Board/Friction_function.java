144a69f37cb6c7b537c7e9d44397f3baebbce212
Louissillekens <56159154+Louissillekens@users.noreply.github.com>
2020-03-18 13:17:42 +0000

    Update Friction_function.java

--- non-graphics-code/src/Board/Friction_function.java
+++ non-graphics-code/src/Board/Friction_function.java
@@ -11,7 +11,6 @@ public class Friction_function implements Function2d{
         out_of_bounds_value = value;
     }
 
-    //must be redone so it works how we want to for friction
     @Override
     public double evaluate(Vector2d p) {
 
@@ -23,18 +22,10 @@ public class Friction_function implements Function2d{
         if (x == Math.floor(x) && y == Math.floor(y)) {
             return frictionMap[(int) x][(int) y];
         }
-        double x_diff = x - Math.floor(x);
-        double y_diff = y - Math.floor(y);
-        double n1 = frictionMap[(int) Math.floor(x)][(int) Math.floor(y)];
-        double n2;
-        if (x >= frictionMap.length-1 || y >= frictionMap[0].length-1) {
-            n2 = 1;
-        }
-        else {
-            n2 =  frictionMap[(int) Math.floor(x + 1)][(int) Math.floor(y + 1)];
-        }
+        double x_diff = Math.floor(x);
+        double y_diff = Math.floor(y);
 
-        return ((x_diff + y_diff)/2) * (n2-n1)+n1;
+        return frictionMap[(int) x_diff][(int) y_diff];
     }
 
     @Override
@@ -42,4 +33,3 @@ public class Friction_function implements Function2d{
         return null;
     }
 }
-
