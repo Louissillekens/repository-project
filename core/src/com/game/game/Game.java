87bb38eb60de1568b427be53ca21999cd07c0d25
ReinKengen <kengenrein@gmail.com>
2020-03-19 12:18:30 +0100

    Update Game.java

--- core/src/com/game/game/Game.java
+++ core/src/com/game/game/Game.java
@@ -12,7 +12,6 @@ import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.MeshBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.MeshPartBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
@@ -21,7 +20,6 @@ import com.badlogic.gdx.math.Vector3;
 public class Game extends ApplicationAdapter implements InputProcessor{
 
 	private PerspectiveCamera camera;
-	private Environment environment;
 
 	@Override
 	public void create () {
@@ -41,12 +39,10 @@ public class Game extends ApplicationAdapter implements InputProcessor{
 		camera.near = 0.1f;
 		camera.far = 300.0f;
 
-		environment = new Environment();
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
 
 	}
-	public void move ball(){
+	public void move_ball(){
 		while(a.get_V()!=(0,0)&&a.get_Vprev()!=(0,0)){
 			a.update_all();
 			//somehow make the ball move to it's new coordinates
@@ -59,8 +55,6 @@ public class Game extends ApplicationAdapter implements InputProcessor{
 	@Override
 	public void render () {
 
-		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 	}
 
@@ -113,22 +107,5 @@ public class Game extends ApplicationAdapter implements InputProcessor{
 	@Override
 	public boolean scrolled(int amount) {
 		return false;
-	}
-
-	@Override
-	public void dispose () {
-
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void resume() {
 	}
 }
