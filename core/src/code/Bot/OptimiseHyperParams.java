f3a01eebcf002379bbb047538648111adf792d9d
Alexandre Martens <56159200+AlexMartens12@users.noreply.github.com>
2020-05-04 16:33:22 +0200

    upd

--- core/src/code/Bot/OptimiseHyperParams.java
+++ core/src/code/Bot/OptimiseHyperParams.java
@@ -40,5 +40,12 @@ public class OptimiseHyperParams {
                 }
             }
         }
+
+        for (int i = 0; i < finalRes.length; i++){
+            System.out.println(Arrays.toString(finalRes[i]));
+            if ((finalRes[i][0] == 0 && finalRes[i][1] == 0)){
+                break;
+            }
+        }
     }
 }
