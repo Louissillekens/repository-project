8389d0001aa7aaf244a6fc1d09427dbc7d9dd2d7
clemdetry <c.detry@student.maastrichtuniversity.nl>
2020-06-15 01:35:01 +0200

    Agent sensors + new features

--- core/src/code/Physics/Rungekuttasolver.java
+++ core/src/code/Physics/Rungekuttasolver.java
@@ -14,13 +14,19 @@ public class Rungekuttasolver{
     private double x;
     private double y;
     private final double onesixth =(double)1/6;
+    private static boolean hasStopped;
 
     public static void main(String args[]){
 
         Rungekuttasolver solver = new Rungekuttasolver();
         solver.setValues(0,0,3.9726,8.519);
-        for(int i=0; i<6000; i++){
+        while (!hasStopped) {
             solver.RK4();
+            System.out.println("x: "+solver.getX());
+            System.out.println("y: "+solver.getY());
+            System.out.println();
+            System.out.println("vx: " + solver.getVx());
+            System.out.println("vy: " + solver.getVy());
         }
         System.out.println("the x and y coordinates are:");
         System.out.println("x: "+solver.getX());
@@ -51,11 +57,11 @@ public class Rungekuttasolver{
 
     public boolean hasBallStopped(){
 
-        boolean stopped =false;
+        hasStopped =false;
         if(vx==0 && vy==0 /*&& ax==0 && ay == 0*/){
-            stopped = true;
+            hasStopped = true;
         }
-        return stopped;
+        return hasStopped;
     }
     public double getHeight(double x, double y){
 
@@ -120,4 +126,11 @@ public class Rungekuttasolver{
         return y;
     }
 
+    public double getVx() {
+        return vx;
+    }
+
+    public double getVy() {
+        return vy;
+    }
 }
