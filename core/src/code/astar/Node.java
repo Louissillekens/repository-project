bbd13c953f121616ff872c885ee0385dd2986da7
Louissillekens <56159154+Louissillekens@users.noreply.github.com>
2020-06-16 15:00:38 +0100

    made a few methods in Node class that we will need

--- core/src/code/astar/Node.java
+++ core/src/code/astar/Node.java
@@ -1,7 +1,5 @@
 package code.astar;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Node {
 
@@ -25,6 +23,10 @@ public class Node {
         checked = false;
         score = 0;
     }
+    //optimisation idea: make a method that creates a shot specifically directed towards the goal
+    // we call this method to make one of the ten shots for each node
+    //this allows us to be sure that when the goal and the ball have no object inbetween
+    // at least one shot will go in the right direction
 
     public void generateShot(){
         //TODO call generate angle and generate power to set these randomly
@@ -56,6 +58,19 @@ public class Node {
         double y_dist = Math.abs(z - this.getZ());
 
         return Math.sqrt((x_dist*x_dist) + (y_dist*y_dist));
+    }
+
+    public double[] executeShot(){
+        //TODO should use one of the solvers to execute the shot and then return the data of the new location
+        //TODO in an array of length 2 (x and y)
+        //temp
+        double[] data ={0,0};
+        return data;
+    }
+
+    public boolean hasParent(){
+        if(this.getParent() == null) return false;
+        return true;
     }
 
     public Node getParent() {
