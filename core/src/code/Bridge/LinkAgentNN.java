cd4b268d8218eed9d2de40ac4f2f01871442f702
Alexandre Martens <56159200+AlexMartens12@users.noreply.github.com>
2020-06-20 14:44:45 +0200

    update for Q learning integration

--- core/src/code/Bridge/LinkAgentNN.java
+++ core/src/code/Bridge/LinkAgentNN.java
@@ -1,33 +1,34 @@
 package code.Bridge;
 
-import java.util.ArrayList;
-
 public class LinkAgentNN {
 
-    private static boolean checkCollision;
-    private static boolean checkWinPosition;
+    private static boolean collision;
+    private static boolean win_position;
     private static float xPosition;
     private static float yPosition;
     private static int action;
 
 
-    public LinkAgentNN(ArrayList<boolean[]> booleansOutput, ArrayList<float[]> dataOutput) {
-
-        boolean[] checkOutputBooleanStep = booleansOutput.get(action);
-        float[] checkOutputDataStep = dataOutput.get(action);
-
-        checkCollision = checkOutputBooleanStep[0];
-        checkWinPosition = checkOutputBooleanStep[1];
-        xPosition = checkOutputDataStep[3];
-        yPosition = checkOutputDataStep[4];
+    public LinkAgentNN(int action){
+        constructorCalc(/* YOUR METHOD THAT RETURNS A FLOAT ARRAY*/);
     }
 
-    public static void setAction(int action) {
-        LinkAgentNN.action = action;
-    }
+    private void constructorCalc(float[] arr){
+        // Collision
+        if (arr[3] == 1)
+            this.collision = true;
+        else
+            this.collision = false;
 
-    public static int getAction() {
-        return action;
+        // Win position
+        if (arr[4] == 1)
+            this.win_position = true;
+        else
+            this.win_position = false;
+
+        // Update the position
+        this.xPosition = arr[5];
+        this.yPosition = arr[6];
     }
 
     public static float getxPosition() {
@@ -38,11 +39,22 @@ public class LinkAgentNN {
         return yPosition;
     }
 
-    public static boolean getcheckCollision() {
-        return checkCollision;
+    public static boolean getCollision() {
+        return collision;
     }
 
-    public static boolean getcheckWinPosition() {
-        return checkWinPosition;
+    public static boolean getWinPosition() {
+        return win_position;
     }
+
+    /*    public LinkAgentNN(ArrayList<boolean[]> booleansOutput, ArrayList<float[]> dataOutput) {
+
+        boolean[] checkOutputBooleanStep = booleansOutput.get(action);
+        float[] checkOutputDataStep = dataOutput.get(action);
+
+        collision = checkOutputBooleanStep[0];
+        win_position = checkOutputBooleanStep[1];
+        xPosition = checkOutputDataStep[3];
+        yPosition = checkOutputDataStep[4];
+    }*/
 }
